dade5ae340deebe04be7d539b0b4b5862d841cc9
Abdulwahab kayode <dahbiigkayode00@gmail.com>
2026-05-05 19:27:54 +0100

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-semibold">Drift Status</h1>
+      <p className="text-gray-500 mt-2">
+        System status & incident tracker
+      </p>
+    </div>
+  )
+}
